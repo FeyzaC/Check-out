d3fcb0b7d8c6ef3a39cb3dc111b54440ff5aeb46
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-01-23 11:06:50 +0100

    calculateProductPrice

--- app.js
+++ app.js
@@ -27,7 +27,35 @@ productsDiv.addEventListener("click", (event) => {
           } will be deleted!!!`
         )
       ) {
+        event.target.closest(".product").remove();
       }
     }
+  } else if (event.target.className == "fa-solid fa-plus") {
+    event.target.parentElement.querySelector(".quantity").innerText++;
+  } else if (event.target.className == "remove-product") {
+    event.target.closest(".product").remove();
   }
+  calculateProductPrice(event.target);
+  calculateCartPrice();
 });
+
+const calculateProductPrice = (btn) => {
+  const productInfoDiv = btn.parentElement.parentElement;
+
+  const price = Number(
+    productInfoDiv.querySelector(".product-price strong").innerText
+  );
+  const quantity = Number(productInfoDiv.querySelector(".quantity").innerText);
+  const productTotalDiv = productInfoDiv.querySelector(".price");
+
+  productTotalDiv.innerText = price * quantity.toFixed(2);
+  const productTotalPricesDiv = document.querySelector(".price");
+  const subtotal = [...productTotalPricesDiv].reduce(
+    (acc, price) => acc + Number(price.innerText)
+  );
+  const taxtPrice = subtotal * localStorage.getItem("taxtRate");
+  subtotal > 0 && subtotal < localStorage.getItem("shippingFreePrice")
+    ? localStorage.getItem("shippingPreice")
+    : 0;
+  const totalCart = subtotal + taxtPrice + shippingPrice;
+};
