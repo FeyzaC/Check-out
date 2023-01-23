2ea8ea595056ae31af62a5d05c7c5bbc180a4f6b
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-01-23 09:45:47 +0100

    Storage

---
+++ app.js
@@ -0,0 +1,13 @@
+const taxtRate = 0.18;
+const shippingPrice = 20;
+const shippingFreePreice = 300;
+
+window.addEventListener("load", () => {
+  localStorage.setItem("taxtRate, taxtRate");
+  localStorage.setItem("shippingPrice", shippingPrice);
+  localStorage.setItem("shippingFreePreice", shippingFreePreice);
+
+  //   sessionStorage.setItem("taxtRate, taxtRate");
+  //   sessionStorage.setItem("shippingPrice", shippingPrice);
+  //   sessionStorage.setItem("shippingFreePreice", shippingFreePreice);
+});
